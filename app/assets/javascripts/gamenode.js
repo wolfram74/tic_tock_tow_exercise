865765d599030e2f97e495580da2e5dc49755030
wolfram74 <phaugen@gmail.com>
2015-05-16 15:48:28 -0700

    refactored check methods

--- app/assets/javascripts/gamenode.js
+++ app/assets/javascripts/gamenode.js
@@ -15,40 +15,40 @@ GameNode.prototype.outcomeCheck = function(){
 
 GameNode.prototype.checkCols = function(){
   var out = 0;
-  var capture = /(\d)\1\1/;
   for(var col=0; col<3; col++){
     var triplet = this.state[col]+this.state[col+3]+this.state[col+6]
-    if (capture.test(triplet)){
-      out = capture.exec(triplet)[1];
-      if (!!out){return parseInt(out)};
-    };
+    out = this.stringCheck(triplet)
+    if (!!out){return parseInt(out)};
   };
-  return parseInt(out)
+  return out
 };
 
 GameNode.prototype.checkRows = function(){
   var out = 0;
-  var capture = /(\d)\1\1/;
   for(var row=0; row<3; row++){
-    var triplet = this.state[row*3]+this.state[row*3+1]+this.state[row*3+2]
-    if (capture.test(triplet)){
-      out = capture.exec(triplet)[1];
-      if (!!out){return parseInt(out)};
-    };
+    var triplet = this.state.substring(row*3,row*3+3)
+    console.log(triplet, row)
+    out = this.stringCheck(triplet)
+    if (!!out){return out};
   };
-  return parseInt(out)
-}
+  return out
+};
 GameNode.prototype.checkDiags = function(){
   var out = 0;
-  var capture = /(\d)\1\1/;
   var trips = [this.state[0]+this.state[4]+this.state[8],
                this.state[2]+this.state[4]+this.state[6]]
   for(var diag=0; diag<3; diag++){
     var triplet = trips[diag]
-    if (capture.test(triplet)){
-      out = capture.exec(triplet)[1];
-      if (!!out){return parseInt(out)};
-    };
+    out = this.stringCheck(triplet)
+    if (!!out){return out};
   };
-  return parseInt(out)
+  return out
+};
+
+GameNode.prototype.stringCheck = function(string){
+  var capture = /(\d)\1\1/;
+  if (capture.test(string)){
+    return parseInt(capture.exec(string)[1]);
+  };
+  return 0
 }
